7459fafee342197f5e57ada9a3020f4379ad1022
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2022-05-12 22:51:58 +0200

    lvl1 changes

---
+++ ScaryMaze/Assets/Scripts/Draggable.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Draggable : MonoBehaviour
+{
+    public Camera myCam;
+    private string nextSceneName;
+    private float startXPos;
+    private float startYPos;
+
+    private bool isDragging = false;
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            DragObject();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (!myCam.orthographic)
+        {
+            mousePos.z = 10;
+        }
+
+        mousePos = myCam.ScreenToWorldPoint(mousePos);
+
+        startXPos = mousePos.x - transform.localPosition.x;
+        startYPos = mousePos.y - transform.localPosition.y;
+
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    public void DragObject()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (!myCam.orthographic)
+        {
+            mousePos.z = 10;
+        }
+
+        mousePos = myCam.ScreenToWorldPoint(mousePos);
+        transform.localPosition = new Vector3(mousePos.x - startXPos, mousePos.y - startYPos, transform.localPosition.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+            SceneManager.LoadScene("GameOver");
+    }
+}
